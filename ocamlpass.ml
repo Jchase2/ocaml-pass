ae12a3b3030379cfd13738d9f67a4ea80494a11a
JChase2 <mr.jdchase@gmail.com>
2017-11-27 23:14:56 -0500

    Updating menu

--- ocamlpass.ml
+++ ocamlpass.ml
@@ -25,6 +25,7 @@ let help = [
     "Type 'read' to read the entire file.";
     "Type 'bsearch' to search for and print a block.";
     "Type 'block' to insert a new password block.";
+    "Type 'rb' to remove a block.";
     "Type 'q' to quit."
 ]
 
